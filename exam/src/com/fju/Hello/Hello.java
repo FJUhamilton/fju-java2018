196b9db4be0291285fe004c19d868df7f1cf9606
FJUhamilton <user@es302n12>
2018-10-02 11:03:40 +0800

    exam

--- exam/src/com/fju/Hello/Hello.java
+++ exam/src/com/fju/Hello/Hello.java
@@ -1,17 +1,23 @@
-package com.tom;
+package com.fju.Hello;
 
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hi Tom.");
+		System.out.println("Hello.");
 		System.out.println("My Name is Matt.");
         int myAge=19;
 		int myHeight=172;
         float myWeight=83.5f;
-
+     
+        System.out.print("My age is ");
         System.out.println(myAge);
+        System.out.print("My height is ");
         System.out.println(myHeight);
+        System.out.print("My weight is ");
         System.out.println(myWeight);
+        System.out.print("Nice to meet you.");
+
+
 
 	}
 
