d13d44217fac5142b9b48b2898e6da93b0ee1986
user <user@es302n35>
2018-10-16 11:57:50 +0800

    matt

--- exam/src/com/fju/Hello/Hello.java
+++ exam/src/com/fju/Hello/Hello.java
@@ -5,7 +5,23 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello.");
 		System.out.println("My Name is Matt.");
-        int myAge=19;
+        
+		Student mg = new Student();
+		mg.english =70;
+		mg.math=90;
+		mg.avg=(mg.english+mg.math)/2;
+		mg.print();
+		
+		
+		
+		/*String name= "Matt";
+		int english=70;
+		int math =90;
+	    int avg=(math+english)/2;
+	    System.out.println(name+"\t"+english+"\t"+math+"\t"+avg);*/
+		
+		
+		/*int myAge=19;
 		int myHeight=172;
         float myWeight=83.5f;
         float myBMI= (10000*myWeight/(myHeight*myHeight));
@@ -17,7 +33,8 @@ public class Hello {
         System.out.println(myWeight);
         System.out.print("My BMI is ");
         System.out.println(myBMI);
-        System.out.print("Nice to meet you.");
+        System.out.print("Nice to meet you.");*/
+        
 
 
 
---
+++ exam/src/com/fju/Hello/Student.java
@@ -0,0 +1,12 @@
+package com.fju.Hello;
+
+public class Student {
+	String name = "mg";
+	int english;
+	int math;
+	int avg;
+
+	public void print() {
+		System.out.println(name + "\t" + english + "\t" + math + "\t" + avg);
+	}
+}
