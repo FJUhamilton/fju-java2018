88f29dc60aa47750a6bdc2adf06fe93d6883050d
user <user@es302n35>
2018-10-23 11:57:13 +0800

    MG

--- exam/src/com/fju/Hello/SignUp.java
+++ exam/src/com/fju/Hello/SignUp.java
@@ -5,11 +5,22 @@ public class SignUp {
 	public static void main(String[] args) {
 	System.out.println("Are you 18?(y/N)");
 	Scanner scanner= new Scanner(System.in);
-	String line = scanner.nextLine();
-	System.out.println("")
-	boolean adult = line.equals();
-	System.out.println(adult);
-	
-	}
+	String tf = scanner.nextLine();
+	System.out.println("*"+tf+"*");
+	boolean adult = tf.toUpperCase().equals("Y");
+	if (adult){
+		System.out.println("What's your age?:");
+        int age =scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("What's your name?:");
+        String name=scanner.nextLine();
+        System.out.println("What's your nickname?:");
+        String nn =scanner.nextLine();
+    System.out.println(age+"\t"+name+"\t"+nn);
 
+}
+	else{
+		System.out.println("BYE =)");
+	}
+	}
 }
