1a65c85bf82a729e052d2d80098c9da584f3acb3
FJUhmailton <matt60601@gmail.com>
2018-11-27 12:06:32 +0800

    hi

---
+++ classNote/src/com/fju/PokerGame.java
@@ -0,0 +1,17 @@
+package com.fju;
+import java.util.Random;
+public class PokerGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Random random= new Random();
+       int num = (random.nextInt(12+1));
+       int length;
+       
+	
+       System.out.print(num);
+      
+       
+	}
+
+}
